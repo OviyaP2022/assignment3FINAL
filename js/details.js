f034944c947370378ae5724831eba6f5052bbd11
OviyaP2022 <113532305+OviyaP2022@users.noreply.github.com>
2024-03-25 10:57:42 -0400

    k

--- js/details.js
+++ js/details.js
@@ -5,5 +5,26 @@ var id = parametersInURL.get('id');
 
 var client = contentful.createClient({
     space: 'ete5e4krj05q',
-    accessToken: 'Bb-Y-ihGV2S_IjpzqfnpOdE5hrgWxqe1l3LDqWLL8-8',
+    accessToken: 'CFPAT-ftGO8_klmWJQqOztYgYOfo0hD9vtItIwGfeXuDREYNs',
   });
+
+  var name = document.createElement('h2'); {
+    name.innerHTML = entry.fields.title; 
+    entryDiv.appendChild(name);
+  }
+
+
+var image = document.createElement('img');
+    if (entry.fields.drinktopping){
+    drinktopping.src = 'https:' + entry.fields.drinktopping.fields.file.url;
+}
+
+var description = document.createElement('p'); {
+    description.innerHTML = entry.fields.description;
+    entryDiv.appendChild(description);
+}
+
+var link = document.createElement('a');
+    link.href = "details.html?id="+entry.sys.id;
+    link.appendChild(drinktopping);
+    entryDiv.appendChild(link);
--- js/script.js
+++ js/script.js
@@ -1,25 +1,41 @@
 var client = contentful.createClient({
     space: 'ete5e4krj05q',
-    accessToken: 'Bb-Y-ihGV2S_IjpzqfnpOdE5hrgWxqe1l3LDqWLL8-8',
+    accessToken: 'ilii4B8XswOHBvJwyDec5UOSf6a5O-gJPNn4WqKrqBM',
   });
 
 var placeForContent = document.getElementById('place-for-boba-content');
 
 // gets all the entries as a json
-client.getEntries({content_type:'wellKnownKPopGroups'}).then(function (entries) {
+client.getEntries({content_type:'bubbleTeaEncyclopedia'}).then(function (entries) {
+    console.log(entries);
     // loops through the json to look at one entry at a 
     entries.items.forEach(function (entry) {
-    // if statement checks that this field exists 
-    var entryDiv = document.createElement('div');
-    entryDiv.classList.add('entry-div'); 
-}
+        // if statement checks that this field exists 
+        var entryDiv = document.createElement('div');
+        entryDiv.classList.add('entry-div'); 
 
-var name = document.createElement('h2');
-    name.innerHTML = entry.fields.groupName;
-    entryDiv.appendChild(name);
-}
+        var name = document.createElement('h2');
+        name.innerHTML = entry.fields.title;
+        entryDiv.appendChild(name);
 
-var groupimage = document.createElement('img');
-    if (entry.fields.drinktopping){
-    drinktopping.src = 'https:' + entry.fields.drinktopping.fields.file.url;
-}
+        var image = document.createElement('img'); 
+        if (entry.fields.drinktopping){
+            image.src = 'https:' + entry.fields.drinktopping.fields.file.url;
+        }
+
+        var description = document.createElement('p'); 
+            description.innerHTML = entry.fields.description;
+            entryDiv.appendChild(description);
+        
+
+        var link = document.createElement('a');
+        link.href = "details.html?id="+entry.sys.id;
+        link.appendChild(image);
+        entryDiv.appendChild(link);
+
+
+        placeForContent.appendChild(entryDiv);
+    });
+
+        
+});
