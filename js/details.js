eb43a1aab888d04ed63ae4a5c3c0d8a3b181054e
OviyaP2022 <113532305+OviyaP2022@users.noreply.github.com>
2024-04-01 09:40:36 -0400

    k

--- js/details.js
+++ js/details.js
@@ -11,25 +11,24 @@ var client = contentful.createClient({
   var type = document.getElementById('type');
   client.getEntry(id).then(function (entry) {
     console.log(entry);
+    var name = document.createElement('h2'); {
+      name.innerHTML = entry.fields.title; 
+      type.appendChild(name);
+    }
+    var image = document.createElement('img'); 
+    if (entry.fields.drinktopping){
+        image.src = 'https:' + entry.fields.drinktopping.fields.file.url;
+        type.appendChild(image);
 
-  var name = document.createElement('h2'); {
-    name.innerHTML = entry.fields.title; 
-    entryDiv.appendChild(name);
-  }
+    }
 
+    var description = document.createElement('p'); {
+        description.innerHTML = entry.fields.description;
+        type.appendChild(description);
+    }
 
-  var image = document.createElement('img'); 
-  if (entry.fields.drinktopping){
-      image.src = 'https:' + entry.fields.drinktopping.fields.file.url;
-  }
-
-var description = document.createElement('p'); {
-    description.innerHTML = entry.fields.description;
-    entryDiv.appendChild(description);
-}
-
-var link = document.createElement('a');
-    link.href = "details.html?id="+entry.sys.id;
-    link.appendChild(drinktopping);
-    entryDiv.appendChild(link);
+    var link = document.createElement('a');
+        link.href = "details.html?id="+entry.sys.id;
+        link.appendChild(drinktopping);
+        type.appendChild(link);
 });
